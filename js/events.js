190cba4739a8624f02bc2511edf0cdc31161ea1b
candescent-mouse-0917 <aqiph818@gmail.com>
2020-02-16 09:28:34 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt(){
 
 //pressIt
 function pressIt(){
-  $('form').on('keydown', (event)=>{
+  $('input').on('keydown', (event)=>{
     if (event.which == 71){
       alert('G is pressed!')
     }
