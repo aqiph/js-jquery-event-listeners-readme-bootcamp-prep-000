8eb1d3ca2c48efdcb4b3355e0cbf48daa8aa8332
candescent-mouse-0917 <aqiph818@gmail.com>
2020-02-16 08:58:32 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -6,7 +6,7 @@ function getIt(){
 //frameIt
 function frameIt(){
   $('img').on('load', function(event){
-    $('img').attr('style', {border-width: 5px; border-color: red;
+    $('img').attr('style', {border-width: "5px"; border-color: red;
   border-style: solid;});
   })
 }
